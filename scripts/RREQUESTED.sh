b300a2ef9330311e0c9194269d5d62092b86b453
AstraBert <133636879+AstraBert@users.noreply.github.com>
2023-09-24 12:01:52 +0200

    Update and rename QUASARd.sh to RREQUESTED.sh

--- scripts/RREQUESTED.sh
+++ scripts/RREQUESTED.sh
@@ -3,19 +3,18 @@
 #QUASARd stands for QUAlity Size and All Reads demultiplexing
 # Usage function
 usage() {
-  echo "Usage: QUASARd -d, --directory RAW_READS_DIRECTORY [-dmx, --demultipex DEMULTIPLEXING_CSV] [-q,--quality QUALITY] [-mi,--min MINIMUM_LENGTH] [-ma,--max MAXIMUM_LENGTH]
+  echo "Usage: RREQ -d, --directory RAW_READS_DIRECTORY [-q,--quality QUALITY] [-mi,--min MINIMUM_LENGTH] [-ma,--max MAXIMUM_LENGTH]
 
   REQUIRED ARGUMENTS:
   -RAW_READS_DIRECTORY: Provide the path to the directory where raw reads are stored in fasta/fasta.gz/fastq/fastq.gz format (can accept also format mixture, but would be better to have all the file in the same format)
 
   OPTIONAL ARGUMENTS:
-  -DEMULTIPLEXING_CSV: Provide a csv file (only comma-separated) whose fields must be "ID,FWD,REV", standing for sample id, forward primer and reverse primer. If not given, no demultiplexing will take place.
   -QUALITY: Provide the value of minimum average read quality for filtering (default: 7)
   -MINIMUM_LENGTH: Provide the minimum length of PCR product for size selection (default is 500)
   -MAXIMUM_LENGTH: Provide the maximum length of PCR product for size selection (default is 10000)
 
   
-  Input QUASARd -h to show this message again"
+  Input RREQ -h to show this message again"
   exit 1
 }
 
@@ -23,17 +22,16 @@ usage() {
 
 # Initialize variables with default values
 directory=""
-demultiplex=""
 quality=7
 min=""
 max=""
 source ~/.bash_aliases
-alias_string=$(alias QUASARd)
+alias_string=$(alias RREQ)
 command_part=$(echo "$alias_string" | awk -F"'" '{print $2}')
 command_part=${command_part#bash }
 absolute_path=$(realpath $(eval echo "$command_part"))
 wd=$(dirname "$absolute_path")
-echo $wd
+
 
 # Loop through the arguments
 while [ $# -gt 0 ]; do
@@ -43,10 +41,6 @@ while [ $# -gt 0 ]; do
             ;;
         -d|--directory)
             directory="$2"
-            shift 2
-            ;;
-        -dmx|--demultiplex)
-            demultiplex="$2"
             shift 2
             ;;
         -q|--quality)
@@ -103,7 +97,8 @@ else
 
     if [ $count_fast -eq 0 ]
     then
-        echo "No fasta/fastq/fasta.gz/fastq.gz files, proceeding with the search of fastq"
+        echo "No fasta/fastq/fasta.gz/fastq.gz files, quitting"
+        exit 1
     else
         mkdir -p ${directory}/results
         echo "Copying files..."
@@ -140,18 +135,11 @@ else
                     dirname=$(dirname "$file")
                     basename_no_ext="${filename%.*}"
                     combined_path="$dirname/$basename_no_ext"
-                    if [ -z "$demultiplex" ]; then
-                        echo "No demultiplexing table provided, the program ended current run"
-                        now=$(date +%Y-%m-%d' '%H:%M:%S)
-                        echo "Program ended current run at ${now}"
-                    else
-                        python3 "${wd}/demultiplex.py" \
-                            -i ${combined_path}_size_selected.fasta \
-                            -p ${demultiplex}
-                        echo "Demultiplexing finished, the program ended current run"
-                        now=$(date +%Y-%m-%d' '%H:%M:%S)
-                        echo "Program ended current run at ${now}"
-                    fi
+                    python3 "${wd}/unref_demult.py" \
+                        -i ${combined_path}_size_selected.fasta
+                    echo "Demultiplexing finished, the program ended current run"
+                    now=$(date +%Y-%m-%d' '%H:%M:%S)
+                    echo "Program ended current run at ${now}"
                 else
                     echo "Program started current run on file $file"
                     python3 "${wd}/quality_filter.py" \
@@ -165,18 +153,11 @@ else
                     dirname=$(dirname "$file")
                     basename_no_ext="${filename%.*}"
                     combined_path="$dirname/$basename_no_ext"
-                    if [ -z "$demultiplex" ]; then
-                        echo "No demultiplexing table provided, the program ended current run"
-                        now=$(date +%Y-%m-%d' '%H:%M:%S)
-                        echo "Program ended current run at ${now}"
-                    else
-                        python3 "${wd}/demultiplex.py" \
-                            -i ${combined_path}_size_selected.fastq \
-                            -p ${demultiplex}
-                        echo "Demultiplexing finished, the program ended current run"
-                        now=$(date +%Y-%m-%d' '%H:%M:%S)
-                        echo "Program ended current run at ${now}"
-                    fi
+                    python3 "${wd}/unref_demult.py" \
+                        -i ${combined_path}_size_selected.fastq
+                    echo "Demultiplexing finished, the program ended current run"
+                    now=$(date +%Y-%m-%d' '%H:%M:%S)
+                    echo "Program ended current run at ${now}"
                 fi
             fi
         done
